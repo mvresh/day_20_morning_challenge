396ee74deafb24fb310311ca250b310e6db3d147
Varun Resh Maddali <mvresh@gmail.com>
2019-10-31 18:14:26 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -11,7 +11,24 @@
 // Generalize this problem by creating a function that accepts the number of
 // soldiers n and the interval at which they are killed i, and returns the
 // position of the fortunate survivor.
+int getSaved(int soldiersNumber, int interval) {
+  List<int> soldiersList = List<int>.generate(soldiersNumber, (i) => i);
+  int killedSoldierIndex = 0;
+  while (soldiersList.length > 1) {
+    killedSoldierIndex =
+        (killedSoldierIndex + interval - 1) % soldiersList.length;
+    soldiersList.removeAt(killedSoldierIndex);
+  }
+  return soldiersList[0] + 1;
+}
 
+//
+//if(soldiersNumber == 1){
+//return 1;
+//}
+//else {
+//return (getSaved(soldiersNumber - 1, interval) + (interval-1)) % soldiersNumber + 1;
+//}
 
 // Challenge 3
 // Movie Theater Seating
@@ -19,7 +36,6 @@
 // where they can sit next to each other in the same row. A movie theater's seat
 // layout can be represented as a 2-D matrix, where 0s represent empty seats and
 // 1s represent taken seats.
-
 
 // Create a function that, given a seat layout and the number of friends n,
 // returns the number of available spots for all n friends to sit together.
@@ -34,5 +50,36 @@
 //  [0, 1, 1, 1, 1, 0, 0]
 //    ], 2) ➞ 3
 
+int groupSeats(List<List> theatre, int n) {
+  int count = 0;
+  theatre.forEach((row) {
+    for(int i =0;i+n <= row.length; i++){
+      row.join('').substring(i,i+n).split('').contains('1') ? null: count++;
+    }
+  });
+  return count;
+}
+
 main() {
+  print(getSaved(41, 3));
+  print(getSaved(2, 1));
+
+  print('------');
+  print(groupSeats([
+[1, 0, 1, 0, 1, 0, 1],
+[0, 1, 0, 1, 0, 1, 0],
+[0, 0, 1, 1, 1, 1, 1],
+[1, 0, 1, 1, 0, 0, 1],
+[1, 1, 1, 0, 1, 0, 1],
+[0, 1, 1, 1, 1, 0, 0]
+], 2));
+
+  print(groupSeats([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+  ], 2));
 }
